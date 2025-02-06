6a906a1c34fc42252fedf085151046d879306cd0
Daniel Bittencourt <danieltartarob@gmail.com>
2025-02-05 21:07:16 -0300

    Aula de Condições I
    Condições simples e compostas

---
+++ aula11/ex009.js
@@ -0,0 +1,9 @@
+var pais = "brasil"
+
+console.log(`Você está em ${pais}`)
+
+if(pais == "brasil"){ //condição composta
+    console.log("você é brasileiro(a)")
+}else{
+    console.log("você é estrangeiro(a)")
+}
